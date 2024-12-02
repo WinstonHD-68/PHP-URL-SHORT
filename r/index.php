39cfe1ffa40ef5448292a71d749fd72fb7cb0a18
WinstonHD-68 <jannickluca2009@gmx.de>
2024-12-02 20:19:26 +0100

    Update index.php

--- r/index.php
+++ r/index.php
@@ -1,1 +1,9 @@
-
+<?php
+require "../classes/class.php";
+if($_GET["a"] != ""){
+    $r = new redirect($_GET["a"]);
+    header("Location: " . $r->redirect());
+}
+else{
+    header("Location: ../");
+}
