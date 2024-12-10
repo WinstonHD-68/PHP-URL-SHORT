3c5125390121327bb0e5f2fa1a96235ce6d31bcc
WinstonHD-68 <jannickluca2009@gmx.de>
2024-12-10 21:27:17 +0100

    Add files via upload

--- r/index.php
+++ r/index.php
@@ -1,1 +1,14 @@
-
+<?php
+require "../classes/class.php";
+if($_GET["a"] != ""){
+    $r = new redirect($_GET["a"]);
+    if(!$r == "error"){
+        header("Location: " . $r->redirect());
+    }
+    else{
+        header("Location: ../?error=2");
+    }
+}
+else{
+    header("Location: ../?error=1");
+}
